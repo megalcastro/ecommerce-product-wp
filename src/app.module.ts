e4d88f3da6c16e7000b4c655f172ef4e39023d1e
Luis Castro <luisd.ing.pro@gmail.com>
2024-12-20 12:33:38 -0500

    install somes packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // o 'postgres'
+      host: 'localhost',
+      port: 5432, // Cambiar si usas PostgreSQL
+      username: '',
+      password: '',
+      database: 'ecommerce',
+      autoLoadEntities: true,
+      synchronize: true, // Cambiar a false en producción
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
